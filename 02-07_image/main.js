14c76de6e58e5d742ca030ca57e2aaefe42ef77e
藤井絢斗 <fujiken20c@gmail.com>
2021-09-07 07:38:52 +0900

    Initial commit

--- 02-07_image/main.js
+++ 02-07_image/main.js
@@ -5,9 +5,28 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+for (let i=0; i<5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic'+(i+1)+'.jpg');
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(e){
+        displayedImage.src = e.target.src;
+    }
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function() {
+    const btnClass = btn.getAttribute('class');
+if (btnClass === 'dark') {
+    btn.setAttribute('class', "light");
+    btn.textContent  = "lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+} else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+
+}
+}
